788b606c6bbe08b6f74cf7c33f0e7bba775f2554
albertoDev <devalberto98@gmail.com>
2025-03-31 11:46:28 +0200

    fixed tests.c

--- src/tests.c
+++ src/tests.c
@@ -4,23 +4,36 @@
 #define WIDTH 256 
 #define HEIGHT 256 
 
-static uint8_t gpx[WIDTH * HEIGHT];
+static uint32_t gpx[WIDTH * HEIGHT];
 
+// NEEDS UPDATING
 static void generate_checkboard_pattern(void)
 {
 	for (int y=0; y<HEIGHT; y++) {
 		for (int x=0; x<WIDTH; x++) {
-			gpx[y * WIDTH + x] = (x ^ y) & 1; // checkboard pattern
+			if ((x+y) % 2 == 0) {
+				gpx[y * WIDTH + x] = 0x000000ff; 
+			} else {
+				gpx[y * WIDTH + x] = 0xffffffff;
+			}
 		}
 	}
 }
 
-static void generate_white_pattern(void)
+// fill gpx with fixed RGBA color
+static void generate_solid_pattern(uint32_t color)
 {
 	for (int y=0; y<HEIGHT; y++) {
 		for (int x=0; x<WIDTH; x++) {
-			gpx[y * WIDTH + x] = 1;
+			gpx[y * WIDTH + x] = color;
 		}
+	}
+}
+
+static void clean_array(void)
+{
+	for (int i=0; i<(WIDTH*HEIGHT); i++) {
+		gpx[i] = 0;
 	}
 }
 
@@ -29,26 +42,17 @@ int test_gpx(void)
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Window *window = SDL_CreateWindow("Monochrome render", WIDTH, HEIGHT, 0);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
-	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_INDEX8, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
-
-	generate_white_pattern();
-
-	SDL_Palette *palette = SDL_CreatePalette(2); // create a 2-color palette
-	SDL_Color colors[2] = {
-		{ 0, 0, 0, 255 }, // index 0: black
-		{ 255, 255, 255, 255 }, // index 1: white
-	};
-	SDL_SetPaletteColors(palette, colors, 0, 2);
-	SDL_SetTexturePalatte(texture, palette); // applay palette to texture
-
-	// convert monochrome to RGB pattern
-	uint8_t pixels[WIDTH * HEIGHT];
-	for (int i=0; i < WIDTH * HEIGHT; i++) {
-		pixels[i] = gpx[i] ? 1 : 0;
-	}
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_BGRA8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
+	
+	uint32_t color_red = 0x0000ffff; // solid red
+	uint32_t color_green = 0x00ff00ff; // solid green
+	uint32_t color_blue = 0xff0000ff; // solid blue 
+	clean_array();
+	generate_checkboard_pattern();
+	// generate_solid_pattern(color_green);
 
 	// update the texture
-	SDL_UpdateTexture(texture, NULL, pixels, WIDTH * sizeof(uint32_t));
+	SDL_UpdateTexture(texture, NULL, gpx, WIDTH * sizeof(uint32_t));
 
 	int running = 1;
 	SDL_Event e;
@@ -59,6 +63,7 @@ int test_gpx(void)
 				running = 0;
 			}
 		}
+		SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, SDL_ALPHA_OPAQUE);
 		SDL_RenderClear(renderer);
 		SDL_RenderTexture(renderer, texture, NULL, NULL);
 		SDL_RenderPresent(renderer);
