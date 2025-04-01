598cc403b37afba4ea058adf7db488327dd25b0a
albertoDev <devalberto98@gmail.com>
2025-04-01 23:22:37 +0200

    implemented draw logic

--- src/display.c
+++ src/display.c
@@ -1,14 +1,23 @@
 #include <stdbool.h>
-
 #include <SDL3/SDL.h>
 
 #include "display.h"
+#include "chip8.h"
 
 /* SDL_Window handler */
 static SDL_Window *window = NULL;
 
-/* SDL_Surface handler */
+/* SDL_Renderer handler */
+static SDL_Renderer *renderer = NULL;
+
+/* SDL_Surface handler */  
 static SDL_Surface *surface = NULL;
+
+/* black and white colors for our monochrome screen */
+static const SDL_Color colors[2] = {
+	{ .r = 0x00, .g = 0x00, .b = 0x00, .a = 0xff }, // black
+	{ .r = 0xff, .g = 0xff, .b = 0xff, .a = 0xff } // white
+};
 
 const unsigned char fonts[80] = {
 	0xf0, 0x90, 0x90, 0x90, 0xf0,	// 0
@@ -29,6 +38,13 @@ const unsigned char fonts[80] = {
 	0xf0, 0x80, 0xf0, 0x80, 0x80,	// F
 };
 
+static void render_texture(SDL_Texture *texture)
+{
+	SDL_RenderClear(renderer); // clear the screen with the default color
+	SDL_RenderTexture(renderer, texture, NULL, NULL); // render the full texture to the full screen
+	SDL_RenderPresent(renderer); // show the rendered image on the screen
+}
+
 /* Initialize SDL graphics library */
 bool graphics_initialize(void)
 {
@@ -42,24 +58,49 @@ bool graphics_initialize(void)
 			SDL_Log("Window could not be created! Error: %s\n", SDL_GetError());
 			success = false;
 		} else {
-			// get window surface
-			surface = SDL_GetWindowSurface(window);
+			// create renderer
+			if (!(renderer = SDL_CreateRenderer(window, NULL))) {
+				SDL_Log("Renderer could not be created! Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			// set renderer default draw color for RenderClear func
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+			// create surface
+			surface = SDL_CreateSurfaceFrom(DISPLAY_WIDTH, DISPLAY_HEIGHT, SDL_PIXELFORMAT_INDEX8, chip8.gpx, DISPLAY_WIDTH * sizeof(char)); 
+			// create palette of 255 entries (because of SDL_PIXELFORMAT_INDEX8) all initialized to white
+			SDL_Palette *palette = SDL_CreateSurfacePalette(surface);
+			// set first two indexes of palette to black and white (we will only use the first two indexes; not very space efficient I know)		
+			if (!SDL_SetPaletteColors(palette, colors, 0, 2)) {
+				SDL_Log("Could not set palette colors! Error: %s\n", SDL_GetError());
+				success = false;
+			}
 		}
 	}
-	
+	return success;
+}
+
+/* main function for updating screen */
+bool draw_frame()
+{
+	bool success = true;
+	// create a texture from surface for exploiting hardware accelaration
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+	// render the texture to the screen
+	render_texture(texture);
+	SDL_DestroyTexture(texture);
 	return success;
 }
 
 /* Cleanup and quit SDL library */
 void graphics_quit(void)
 {
-	// Destroy surface
 	SDL_DestroySurface(surface);
-	
-	// Destroy window
+	// Free renderer
+	SDL_DestroyRenderer(renderer);
+	// Free window
 	SDL_DestroyWindow(window);
-
 	// Quit SDL subsystems
 	SDL_Quit();
 }
+
 
